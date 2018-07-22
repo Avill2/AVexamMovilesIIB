9480afa0ca6aa5f346b083f1a4ddbb7c7eac4d8d
Avill2 <andrea.villacis@epn.edu.ec>
2018-07-22 08:16:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/andreavillacis/av_exammoviles_iib/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.andreavillacis.av_exammoviles_iib
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
